a96a41d5db01e2ae8e22f90263b94b2510bbc7f1
AnnaGoodman1 <anna.goodman@lshtm.ac.uk>
2019-06-29 14:54:51 +0100

    lsoa AP levels

--- 1_ProcessRaster_APextract.R
+++ 1_ProcessRaster_APextract.R
@@ -18,30 +18,42 @@ apraster_bno2 <- raster(apdata_bno2, xmn=-50000, xmx=762000, ymn=-50000, ymx=132
 apdata_bpm25 <- data.matrix(apdata_bpm25, rownames.force = NA)
 apraster_bpm25 <- raster(apdata_bpm25, xmn=-50000, xmx=762000, ymn=-50000, ymx=1328000, crs="+init=epsg:27700")
 
-# Load LAD boundaries
-lad14shape <- readOGR(file.path("../mh-route-commutes/01_DataInput/lad14_boundaries/Local_Authority_Districts_December_2015_Ultra_Generalised_Clipped_Boundaries_in_Great_Britain.shp")) # [NB LAD15CD always same as LAD14CD]
-lad14shape <- spTransform(lad14shape, proj_27700)
-# plot(apraster_bpm25)
-# plot(lad14shape, bg="transparent", add=TRUE)
+## SELECT 1 ##
+# Load boundaries, option 1: LA
+area_shape <- readOGR(file.path("../mh-route-commutes/01_DataInput/lad14_boundaries/Local_Authority_Districts_December_2015_Ultra_Generalised_Clipped_Boundaries_in_Great_Britain.shp")) # [NB LAD15CD always same as LAD14CD]
+area_shape <- spTransform(area_shape, proj_27700)
+area_list <- read.csv("01_DataInput/lad14cd_apgroups.csv")
+#area_list <- read.csv("01_DataInput/lad14cd_separate.csv")
+area_shape@data <- left_join(area_shape@data, area_list, by = "lad15cd")
 
-# Define LA groupings
-lad14 <- read.csv("01_DataInput/lad14cd_apgroups.csv")
-lad14shape@data <- left_join(lad14shape@data, lad14, by = "lad15cd")
-lad14shape <- lad14shape[!is.na(lad14shape@data$apgroup_num),]      # restrict to England
-lad14shape <- ms_dissolve(lad14shape, field = "apgroup_num") 
+# # Load boundaries, option 2: LSOA
+# area_shape <- readOGR(file.path("01_DataInput/lsoa_boundaries/Lower_Layer_Super_Output_Areas_December_2011_Super_Generalised_Clipped__Boundaries_in_England_and_Wales.shp"))
+# area_shape <- spTransform(area_shape, proj_27700)
+# area_list <- read.csv("01_DataInput/lsoa_separate.csv")
+# area_shape@data <- left_join(area_shape@data, area_list, by = "lsoa11cd")
+###############
 
-# Calculate average value by LA grouping
-apgroup_mean <- unique(lad14[,names(lad14) %in% c("apgroup_num", "apgroup_name")])
+# Merge area groupings
+area_shape <- area_shape[!is.na(area_shape@data$apgroup_num),]      # restrict to England
+area_shape <- ms_dissolve(area_shape, field = "apgroup_num") 
+
+# Calculate average value by area
+apgroup_mean <- unique(area_list[,names(area_list) %in% c("apgroup_num", "apgroup_name")])
 numlist <- unique(apgroup_mean$apgroup_num)
 
 apgroup_mean$apmean_bno2 <- NA
 apgroup_mean$apmean_bpm25 <- NA
 for(j in numlist){
-  no2list <- extract(apraster_bno2, lad14shape[lad14shape@data$apgroup_num==j,])
+  no2list <- extract(apraster_bno2, area_shape[area_shape@data$apgroup_num==j,])
   apgroup_mean$apmean_bno2[apgroup_mean$apgroup_num==j] <- round(as.numeric(lapply(no2list, FUN=mean)), 3)
-  pm25list <- extract(apraster_bpm25, lad14shape[lad14shape@data$apgroup_num==j,])
+  pm25list <- extract(apraster_bpm25, area_shape[area_shape@data$apgroup_num==j,])
   apgroup_mean$apmean_bpm25[apgroup_mean$apgroup_num==j] <- round(as.numeric(lapply(pm25list, FUN=mean)), 3)
 }
 
-# Save file
+## SELECT 2 ##
+# Save file, option 1: LA
 write.csv(apgroup_mean, file = file.path("../mh-execute/inputs/background-air-pollution/1_apmeans.csv"), row.names=FALSE)
+#write.csv(apgroup_mean, file = file.path("02_DataCreated/2_ladmeans.csv"), row.names=FALSE)
+
+# Save file, option 2: LSOA
+#write.csv(apgroup_mean, file = file.path("02_DataCreated/1_lsoameans.csv"), row.names=FALSE)
